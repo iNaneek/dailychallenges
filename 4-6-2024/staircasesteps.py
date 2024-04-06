72b8c01c7e58bc8e10d13ab9668ceae83fef8093
iNaneek <Keenan.manchester@gmail.com>
2024-04-06 15:43:22 -0700

    finished 4/6 to be working with example

--- 4-6-2024/staircasesteps.py
+++ 4-6-2024/staircasesteps.py
@@ -10,9 +10,31 @@ def getStairCount():
 
 
 stepOptions = (1, 2)
-stairs = 5
+stairs = 4
 #stairs = getStairCount()
 largestCount = math.ceil(stairs/min(stepOptions))
+#largestCount -= 1
+solutions = 0
 
-def loops():
-    pass #
+options = []
+def findAllOptions(wantedLength, stepOptions, currentVal = 0):
+
+    if wantedLength == 0:
+        return None
+
+    for i in stepOptions:
+        options.append(currentVal + i)
+        findAllOptions(wantedLength - 1, stepOptions, currentVal + i)
+
+findAllOptions(largestCount, stepOptions)
+print(options)
+print(len(options))
+
+
+for i in options:
+    if i == stairs:
+        solutions += 1
+
+print(solutions)
+
+
