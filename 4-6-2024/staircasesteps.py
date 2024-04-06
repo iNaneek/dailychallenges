1ac5dfe02c86a244094029d97d7420dc759cd570
iNaneek <Keenan.manchester@gmail.com>
2024-04-06 14:57:15 -0700

    started day 4/6 setup

---
+++ 4-6-2024/staircasesteps.py
@@ -0,0 +1,15 @@
+import math
+def getStairCount():
+    stairCount = None
+    while stairCount == None:
+        try:
+            stairCount = int(input("How many stairs:"))
+        except:
+            print("Please give integer value")
+    return stairCount
+
+
+stepOptions = (1, 2)
+stairs = 5
+#stairs = getStairCount()
+largestCount = math.ceil(stairs/min(stepOptions))
